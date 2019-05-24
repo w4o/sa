54d791fa571485b09ec023746e2d5ed9d78112b2
Frank <no-mail>
2019-05-24 11:24:59 +0800

    fix[admin]:在某个路由下刷新页面404，“错误路由(404)” 放到路由表最后解决。

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -53,10 +53,7 @@ export const constantRoutes = [
       component: () => import('@/views/dashboard/index'),
       meta: { title: 'Dashboard', icon: 'dashboard' }
     }]
-  },
-
-  // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
+  }
 ]
 
 /**
@@ -248,7 +245,10 @@ export const asyncRoutes = [
         meta: { title: 'External Link', icon: 'link' }
       }
     ]
-  }
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
 ]
 
 const createRouter = () => new Router({
